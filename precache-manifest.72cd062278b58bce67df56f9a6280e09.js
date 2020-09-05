3d814a7dd1f9b0758a899ccf32e4a3e50ff6f39c
Sanjiv Pradhanang <pradhanang.sanjiv@gmail.com>
2020-09-05 13:34:04 -0500

    Updates

---
+++ precache-manifest.72cd062278b58bce67df56f9a6280e09.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "02dc6cabb97d624366f23d48e5f8dd7b",
+    "url": "/portfolio/public/index.html"
+  },
+  {
+    "revision": "9fc3acf696469a6630ff",
+    "url": "/portfolio/public/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "e44729af9527b5886190",
+    "url": "/portfolio/public/static/js/2.2494d31c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio/public/static/js/2.2494d31c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9fc3acf696469a6630ff",
+    "url": "/portfolio/public/static/js/main.2b72d932.chunk.js"
+  },
+  {
+    "revision": "01a191e7a152e2f85d81",
+    "url": "/portfolio/public/static/js/runtime-main.68ce4fb2.js"
+  }
+]);
