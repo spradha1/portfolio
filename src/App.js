0c3ba413e645634109028093a7c4b20f9cd6286c
Sanjiv Pradhanang <pradhanang.sanjiv@gmail.com>
2020-09-13 13:15:12 -0500

    Profile links on top-right

--- src/App.js
+++ src/App.js
@@ -1,6 +1,14 @@
 import React, { Component } from 'react';
 import me from './assets/me.jpg';
 import './App.css';
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { fab } from '@fortawesome/free-brands-svg-icons'
+import { far } from '@fortawesome/free-regular-svg-icons'
+library.add(
+  fab,
+  far
+);
 
 class App extends Component {
 
@@ -9,7 +17,18 @@ class App extends Component {
       <div className="App">
         <div className="centerPiece">
           <img src={me} alt="my portrait" />
-          <p>SANJIV PRADHANANG!</p>
+          <p>IT'S SANJIV PRADHANANG!</p>
+        </div>
+        <div className="Profiles">
+          <div><a href="https://github.com/spradha1" target="_blank" rel="noopener noreferrer">
+            <FontAwesomeIcon icon={["fab", "github"]} size="lg" />
+          </a></div>
+          <div><a href="https://www.linkedin.com/in/sanjiv-pradhanang-a05967b5/" target="_blank" rel="noopener noreferrer">
+            <FontAwesomeIcon icon={["fab", "linkedin"]} size="lg" />
+          </a></div>
+          <div><a href="mailto:pradhanang.sanjiv@gmail.com" >
+            <FontAwesomeIcon icon={["far", "paper-plane"]} size="lg" />
+          </a></div>
         </div>
       </div>
     )
