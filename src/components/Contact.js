a4b80e853b681dc8a7956f71edd5f1ce26982788
Sanjiv Pradhanang <pradhanang.sanjiv@gmail.com>
2021-04-19 18:39:53 -0500

    Email functionality for contact form added

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import * as emailjs from 'emailjs-com';
+import {emailConfig} from '../email';
 
 
 class Contact extends Component {
@@ -9,18 +11,74 @@ class Contact extends Component {
 
     };
     this.handleSubmit = this.handleSubmit.bind(this);
+    this.verifyEmail = this.verifyEmail.bind(this);
     this.sendEmail = this.sendEmail.bind(this);
   }
 
 
+  // triggered by form submit button
   handleSubmit = (event) => {
-    alert('Your message is sent to Sanjiv!');
     event.preventDefault();
+    const name = document.querySelector('#name').value;
+    const email = document.querySelector('#email').value;
+    const message = document.querySelector('#message').value;
+
+    const formError = document.querySelector('.form-error');
+    formError.innerHTML = "";
+    var formFailed = false;
+
+    if (name === "" || email === "" || message === "") {
+      formError.innerHTML += "Some value is missing.<br>";
+      formFailed = true;
+    }
+    if (email.length > 0) {
+      if (!this.verifyEmail(email)) {
+        formFailed = true;
+      }
+    }
+
+    if (!formFailed) {
+      this.sendEmail(name, email, message);
+    }
   }
 
-  // send email to the webmaster with all submission information
-  sendEmail = () => {
 
+  // verify email pattern
+  verifyEmail = (email) => {
+    const formError = document.querySelector('.form-error');
+    const emailRegex = /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/;
+    const matches = email.match(emailRegex);
+
+    if (email.length === 0 || !matches) {
+      formError.innerHTML += "Email address is not valid.";
+      return false;
+    }
+    return true;
+  }
+
+
+  // send email to the webmaster with all submission information
+  sendEmail = (name, email, message) => {
+    let emailParams = {
+      from_name: name,
+      from_email: email,
+      to_name: 'Sanjiv',
+      message: message
+    }
+
+    emailjs.send(
+      emailConfig.service_id,
+      emailConfig.template_id,
+      emailParams,
+      emailConfig.user_id
+    ).then(function(response) {
+      alert('Your message is sent to Sanjiv!');
+      document.querySelector('#name').value = "";
+      document.querySelector('#email').value = "";
+      document.querySelector('#message').value = "";
+    }, function(error) {
+      alert('Your message failed to send. If problem persists, use the leftmost option on the top menu to use your email client to send me a message.');
+    });
   }
 
 
@@ -28,21 +86,21 @@ class Contact extends Component {
     return (
       <div className="Contactmain">
         <h3>CONTACT ME</h3>
-        <p>You can reach out to me by filling out the form below. You can also email me through the option provided on the top menu, and check out my GitHub and LinkedIn profiles as well.</p>
+        <p>You can reach out to me by filling out the form below and I will try to get back to you ASAP. You can also email me through your own mail client through the leftmost option provided on the top menu, and check out my GitHub and LinkedIn profiles as well.</p>
         <form onSubmit={this.handleSubmit}>
           <div>
             <label>Name*</label>
-            <input className="formvalue" type="text" />
+            <input className="formvalue" id="name" type="text" />
           </div>
           <div>
             <label>Email*</label>
-            <input className="formvalue" type="text" />
+            <input className="formvalue" id="email" type="text" />
           </div>
           <div>
             <label>Message*</label>
-            <textarea className="formvalue" placeholder="Type your message"></textarea>
+            <textarea className="formvalue" id="message" placeholder="Type your message"></textarea>
           </div>
-          <div className='form-failed-alert'></div>
+          <div className='form-error'></div>
           <div className="submitbutton">
             <input type="submit" value="SUBMIT" />
           </div>
